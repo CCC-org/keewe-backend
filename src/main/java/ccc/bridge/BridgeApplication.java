34bb3f550da2131941cccbf74e7416d6f23c4dba
kimyoungi99 <kimyoungi99@naver.com>
2022-06-13 22:06:25 +0900

    feat : Spring Project Init

---
+++ src/main/java/ccc/bridge/BridgeApplication.java
@@ -0,0 +1,13 @@
+package ccc.bridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BridgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ccc/bridge/BridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package ccc.bridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
