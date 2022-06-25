a6d2dae53d4bfa492fca182011254c68e112c1a4
heoboseong7 <boseong844@gmail.com>
2022-06-25 20:49:19 +0900

    [Feat] restdocs util 추가

--- bridge-api/src/test/java/ccc/bridgeapi/document/utils/ControllerTest.java
+++ bridge-api/src/test/java/ccc/bridgeapi/document/utils/ControllerTest.java
@@ -1,2 +1,19 @@
-package ccc.bridgeapi.document.utils;public class ControllerTest {
+package ccc.bridgeapi.document.utils;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Disabled;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@Disabled
+@WebMvcTest(controllers = {}) // Controller.class 들 입력
+public abstract class ControllerTest {
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @Autowired
+    protected MockMvc mockMvc;
 }
--- bridge-api/src/test/java/ccc/bridgeapi/document/utils/RestDocsTestSupport.java
+++ bridge-api/src/test/java/ccc/bridgeapi/document/utils/RestDocsTestSupport.java
@@ -1,2 +1,35 @@
-package ccc.bridgeapi.document.utils;public class RestDocsTestSupport {
+package ccc.bridgeapi.document.utils;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+@Disabled
+@Import(RestDocsConfig.class)
+@ExtendWith(RestDocumentationExtension.class)
+public class RestDocsTestSupport extends ControllerTest {
+
+    @Autowired
+    protected RestDocumentationResultHandler restDocs;
+
+    @BeforeEach
+    void setUp(final WebApplicationContext context,
+               final RestDocumentationContextProvider provider) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(MockMvcRestDocumentation.documentationConfiguration(provider))
+                .alwaysDo(MockMvcResultHandlers.print())
+                .alwaysDo(restDocs)
+                .addFilters(new CharacterEncodingFilter("UTF-8", true))
+                .build();
+    }
 }
