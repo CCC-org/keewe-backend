b06e72929682df3cad756d087ce3dfcfc5ba9aea
Young-Ki Kim <kimyoungi99@naver.com>
2022-07-04 18:04:18 +0900

    [Fix] Local 환경 datasource H2로 수정 (#33)
    * refactor : 로컬 환경 시 H2-Database
    
    * fix : application-domain.yml secret 생성

--- keewe-domain/src/main/resources/schema.sql
+++ keewe-domain/src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS user
+CREATE TABLE IF NOT EXISTS `user`
 (
     user_id             BIGINT(20)      AUTO_INCREMENT,
     email               VARCHAR(255),
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS user
     PRIMARY KEY(user_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS profile_photo
+CREATE TABLE IF NOT EXISTS `profile_photo`
 (
     profile_photo_id    BIGINT(20)      NOT NULL        AUTO_INCREMENT,
     deleted             BIT             NOT NULL,
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS profile_photo
     PRIMARY KEY (profile_photo_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS profile
+CREATE TABLE IF NOT EXISTS `profile`
 (
     profile_id          BIGINT(20)      NOT NULL        AUTO_INCREMENT,
     user_id             BIGINT(20)      NOT NULL,
@@ -35,11 +35,11 @@ CREATE TABLE IF NOT EXISTS profile
     updated_at          DATETIME(6)     NOT NULL,
 
     PRIMARY KEY (profile_id),
-    FOREIGN KEY (user_id) REFERENCES user(user_id),
-    FOREIGN KEY (profile_photo_id) REFERENCES  profile_photo(profile_photo_id)
+    FOREIGN KEY (user_id) REFERENCES `user`(user_id),
+    FOREIGN KEY (profile_photo_id) REFERENCES  `profile_photo`(profile_photo_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS buddy
+CREATE TABLE IF NOT EXISTS `buddy`
 (
     buddy_id            BIGINT(20)      NOT NULL        AUTO_INCREMENT,
     follower_id         BIGINT(20)      NOT NULL,
@@ -48,22 +48,22 @@ CREATE TABLE IF NOT EXISTS buddy
     updated_at          DATETIME(6)     NOT NULL,
 
     PRIMARY KEY (buddy_id),
-    FOREIGN KEY (followee_id) REFERENCES profile(profile_id),
-    FOREIGN KEY (followee_id) REFERENCES profile(profile_id)
+    FOREIGN KEY (followee_id) REFERENCES `profile`(profile_id),
+    FOREIGN KEY (followee_id) REFERENCES `profile`(profile_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS favorite_activities
+CREATE TABLE IF NOT EXISTS `favorite_activities`
 (
     profile_id      BIGINT(20)      NOT NULL,
     activity        VARCHAR(255)    NOT NULL,
     created_at      DATETIME(6)     NOT NULL,
     updated_at      DATETIME(6)     NOT NULL,
 
-    FOREIGN KEY (profile_id) REFERENCES profile(profile_id),
-    CONSTRAINT UNIQUE (profile_id, activity)
+    FOREIGN KEY (profile_id) REFERENCES `profile`(profile_id),
+    CONSTRAINT `favorite_activities_constraint` UNIQUE (profile_id, activity)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS profile_link
+CREATE TABLE IF NOT EXISTS `profile_link`
 (
     profile_link_id     BIGINT(20)      NOT NULL        AUTO_INCREMENT,
     profile_id          BIGINT(20)      NOT NULL,
@@ -74,5 +74,5 @@ CREATE TABLE IF NOT EXISTS profile_link
     updated_at          DATETIME(6)     NOT NULL,
 
     PRIMARY KEY (profile_link_id),
-    FOREIGN KEY (profile_id) REFERENCES profile(profile_id)
+    FOREIGN KEY (profile_id) REFERENCES `profile`(profile_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
