c0fef30377ab99e2d9760055b1bf23d5f5eacc8c
heoboseong7 <boseong844@gmail.com>
2022-07-09 22:15:52 +0900

    [Fix] 불필요한 import 제거

--- keewe-api/src/main/java/ccc/keeweapi/KeeweApiApplication.java
+++ keewe-api/src/main/java/ccc/keeweapi/KeeweApiApplication.java
@@ -3,7 +3,6 @@ package ccc.keeweapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @ComponentScan({"ccc.keewedomain", "ccc.keeweapi"})
