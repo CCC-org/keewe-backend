5afb65dc83facd8f91b0dfce3f92dd95873e33fb
heoboseong7 <boseong844@gmail.com>
2022-07-10 12:50:12 +0900

    [Fix] 테스트용 UserController 삭제

--- keewe-api/src/main/java/ccc/keeweapi/api/user/UserController.java
+++ keewe-api/src/main/java/ccc/keeweapi/api/user/UserController.java
@@ -1,26 +1,11 @@
 package ccc.keeweapi.api.user;
 
-import ccc.keeweapi.config.security.jwt.JwtUtils;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
 public class UserController {
-
-    private final JwtUtils jwtUtils;
-
-    @PostMapping
-    public String tempLogin(@RequestBody String email) {
-        System.out.println("email = " + email);
-        List<String> roles = new ArrayList<>();
-        return jwtUtils.createToken(email, roles);
-    }
 }
