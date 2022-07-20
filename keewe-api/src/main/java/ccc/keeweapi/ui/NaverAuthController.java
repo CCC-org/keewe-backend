13041ef23cc398590d7567ebf6ae3446c8910ad7
heoboseong7 <boseong844@gmail.com>
2022-07-20 19:35:16 +0900

    [Chore] 로그인 요청 URL UriComponent로 생성

--- keewe-api/src/main/java/ccc/keeweapi/ui/NaverAuthController.java
+++ keewe-api/src/main/java/ccc/keeweapi/ui/NaverAuthController.java
@@ -5,7 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
@@ -31,6 +34,22 @@ public class NaverAuthController {
     @Value("${naver.state-name}")
     private String naverState;
 
+    private String responseType = "code";
+
+    private String authRequestUrl;
+
+    @PostConstruct
+    private void initAuthRequestUrl() {
+        UriComponents uri = UriComponentsBuilder.fromUriString(naverUrl)
+                .pathSegment("oauth2.0", "authorize")
+                .queryParam("response_type", responseType)
+                .queryParam("client_id", clientId)
+                .queryParam("redirect_uri", redirectUrl)
+                .build();
+
+        authRequestUrl = uri.toString();
+    }
+
     public NaverAuthController() throws NoSuchAlgorithmException {
         this.secureRandom = SecureRandom.getInstance("SHA1PRNG");
     }
@@ -39,7 +58,7 @@ public class NaverAuthController {
     public void redirect(HttpSession session, HttpServletResponse response) throws IOException {
         String state = generateState();
         session.setAttribute(naverState, state);
-        response.sendRedirect(naverUrl + "/oauth2.0/authorize?response_type=code&client_id=" + clientId + "&redirect_uri=" + redirectUrl + "&state=" + state);
+        response.sendRedirect(authRequestUrl + "&state=" + state);
     }
 
     private String generateState() {
