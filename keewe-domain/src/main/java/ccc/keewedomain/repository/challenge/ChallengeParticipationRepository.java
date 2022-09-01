01bc4f1d75a33f8ee110f6ae33311a3b01965cb8
heoboseong7 <boseong844@gmail.com>
2022-09-01 15:41:24 +0900

    [Feat] 참여 여부 조회 쿼리 상태 확인하도록 수정

--- keewe-domain/src/main/java/ccc/keewedomain/repository/challenge/ChallengeParticipationRepository.java
+++ keewe-domain/src/main/java/ccc/keewedomain/repository/challenge/ChallengeParticipationRepository.java
@@ -1,14 +1,13 @@
 package ccc.keewedomain.repository.challenge;
 
 import ccc.keewedomain.domain.challenge.ChallengeParticipation;
+import ccc.keewedomain.domain.challenge.enums.ChallengeParticipationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
-import java.time.LocalDate;
 
 public interface ChallengeParticipationRepository extends JpaRepository<ChallengeParticipation, Long> {
     //TODO querydsl 적용 필요
     @Query("select (count(c) > 0) from ChallengeParticipation c " +
-            "where c.challenge.id = ?1 and c.endDate > ?2 and c.deleted = false")
-    boolean existsByChallengeIdAndEndDateAfterAndDeletedFalse(Long challengerId, LocalDate date);
+            "where c.challenger.id = ?1 and c.status = ?2")
+    boolean existsByChallengerIdAndStatus(Long challengerId, ChallengeParticipationStatus status);
 }
