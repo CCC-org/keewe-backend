be1db765617d59aaa7c51473dc26a135fdf23d3e
heoboseong7 <boseong844@gmail.com>
2022-09-06 16:47:18 +0900

    [Fix] 미사용 import 삭제

--- keewe-api/src/main/java/ccc/keeweapi/api/KeeweControllerAdvice.java
+++ keewe-api/src/main/java/ccc/keeweapi/api/KeeweControllerAdvice.java
@@ -4,7 +4,6 @@ import ccc.keeweapi.dto.ApiResponse;
 import ccc.keewecore.consts.KeeweRtnConsts;
 import ccc.keewecore.exception.KeeweException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
