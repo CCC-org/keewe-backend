8e5d317f09f33df60b1e4409f686ef31b808b146
winter-you <winter.you@kakaoenterprise.com>
2022-09-11 15:26:54 +0900

    [Feat] Local mysql shell script 추가

---
+++ scripts/docker_mysql_macosm1.sh
@@ -0,0 +1,13 @@
+sudo mkdir -p /private/keewe/mysql
+sudo chmod -R a+rwx /private/keewe/mysql
+docker run --name keewe-mysql \
+    -e MYSQL_USER=keewe \
+    -e MYSQL_PASSWORD=keewee \
+    -e MYSQL_DATABASE=keewe \
+    -e MYSQL_ALLOW_EMPTY_PASSWORD=yes \
+    -e LANG=C.UTF-8 \
+    --platform linux/amd64 \
+    -d -p 3306:3306 \
+    -v /private/keewe/mysql:/var/lib/mysql:rw \
+    --user "1000:50" \
+    mysql:5.7
