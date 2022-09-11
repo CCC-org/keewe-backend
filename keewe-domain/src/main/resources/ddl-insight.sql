4eb4320aed8490feadadb3dba8979440cf3d9bf4
winter-you <winter.you@kakaoenterprise.com>
2022-09-11 13:09:13 +0900

    [Feat] Reaction, Reaction Aggregation 테이블정의

--- keewe-domain/src/main/resources/ddl-insight.sql
+++ keewe-domain/src/main/resources/ddl-insight.sql
@@ -13,3 +13,21 @@ CREATE TABLE IF NOT EXISTS `insight`
     FOREIGN KEY (writer_id) REFERENCES `user`(user_id),
     FOREIGN KEY (challenge_participation_id) REFERENCES `challenge_participation`(challenge_participation_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE IF NOT EXISTS 'reaction'
+(
+    insight_id    BIGINT      NOT NULL,
+    reactor_id    BIGINT      NOT NULL,
+    reaction_type VARCHAR(15) NOT NULL,
+
+    PRIMARY KEY (insight_id, reactor_id, reaction_type)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE IF NOT EXISTS 'reaction_aggregation'
+(
+    insight_id             BIGINT      NOT NULL ,
+    reaction_type          VARCHAR(15) NOT NULL,
+    count                  BIGINT      NOT NULL  DEFAULT 0
+)
