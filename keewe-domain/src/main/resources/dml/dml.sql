63a47a1af424cc5edea6118b79598d581fc7b84b
winter-you <winter.you@kakaoenterprise.com>
2022-09-15 22:02:53 +0900

    dml 수정

--- keewe-domain/src/main/resources/dml/dml.sql
+++ keewe-domain/src/main/resources/dml/dml.sql
@@ -1,4 +1,4 @@
 
 INSERT INTO `user` (user_id, email, password, phone_number, status, deleted, created_at, updated_at, privacy, vendor_id, vendor_type)
-VALUES(0, 'test@keewe.com', 'pass', '01012341234', 'ONBOARDING', false, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'PUBLIC', '0000001', 'kakao');
+VALUES(0, 'test@keewe.com', 'pass', '01012341234', 'ONBOARD', false, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'PUBLIC', '0000001', 'KAKAO');
 
