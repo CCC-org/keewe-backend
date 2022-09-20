69bdb6826bcc151fc2905f6b6c96c43669f5b6f0
heoboseong7 <boseong844@gmail.com>
2022-09-20 13:36:46 +0900

    [Test] 서랍 등록 API 테스트

--- keewe-api/src/test/java/ccc/keeweapi/api/insight/DrawerControllerTest.java
+++ keewe-api/src/test/java/ccc/keeweapi/api/insight/DrawerControllerTest.java
@@ -1,8 +1,8 @@
 package ccc.keeweapi.api.insight;
 
 import ccc.keeweapi.document.utils.ApiDocumentationTest;
-import ccc.keeweapi.dto.insight.InsightCreateResponse;
-import ccc.keeweapi.service.insight.InsightApiService;
+import ccc.keeweapi.dto.insight.DrawerCreateResponse;
+import ccc.keeweapi.service.insight.DrawerApiService;
 import com.epages.restdocs.apispec.ResourceSnippetParameters;
 import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
@@ -23,57 +23,48 @@ import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuild
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-public class InsightControllerTest extends ApiDocumentationTest {
+public class DrawerControllerTest extends ApiDocumentationTest {
 
     @InjectMocks
-    InsightController insightController;
+    DrawerController drawerController;
 
     @Mock
-    InsightApiService insightApiService;
+    DrawerApiService drawerApiService;
 
     @BeforeEach
-    public void setup(RestDocumentationContextProvider provider) {
-        super.setup(insightController, provider);
+    void setup(RestDocumentationContextProvider provider) {
+        super.setup(drawerController, provider);
     }
 
     @Test
-    @DisplayName("인사이트 등록 API")
+    @DisplayName("서랍 등록 API")
     void create_insight_test() throws Exception {
-        String contents = "내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용";
-        String url = "https://tech.kakao.com/2022/03/17/2022-newkrew-onboarding-codereview/";
-        boolean participation = false;
-        Long drawerId = 1L;
+        String drawerName = "개발";
 
-        JSONObject insightCreateRequest = new JSONObject();
-        insightCreateRequest
-                .put("contents", contents)
-                .put("url", url)
-                .put("participation", participation)
-                .put("drawerId", drawerId);
+        JSONObject drawerCreateRequest = new JSONObject();
+        drawerCreateRequest
+                .put("name", drawerName);
 
-        when(insightApiService.create(any())).thenReturn(InsightCreateResponse.of(1L));
+        when(drawerApiService.create(any())).thenReturn(DrawerCreateResponse.of(1L));
 
-        ResultActions resultActions = mockMvc.perform(post("/api/v1/insight")
+        ResultActions resultActions = mockMvc.perform(post("/api/v1/drawer")
                         .header(HttpHeaders.AUTHORIZATION, "Bearer " + JWT)
-                        .content(insightCreateRequest.toString())
+                        .content(drawerCreateRequest.toString())
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
 
         resultActions.andDo(restDocs.document(resource(
                 ResourceSnippetParameters.builder()
-                        .description("인사이트 생성 API 입니다.")
-                        .summary("인사이트 생성 API")
+                        .description("서랍 생성 API 입니다.")
+                        .summary("서랍 생성 API")
                         .requestHeaders(
                                 headerWithName("Authorization").description("유저의 JWT"))
                         .requestFields(
-                                fieldWithPath("contents").description("인사이트의 내용. 최대 300자, 문자 제한 없음"),
-                                fieldWithPath("url").description("등록한 링크의 url. 최대 2000자"),
-                                fieldWithPath("participation").description("현재 진행중인 챌린지에 참가할 지 여부"),
-                                fieldWithPath("drawerId").description("서랍의 ID. 선택").optional())
+                                fieldWithPath("name").description("서랍의 이름. 15자 이내"))
                         .responseFields(
                                 fieldWithPath("message").description("요청 결과 메세지"),
                                 fieldWithPath("code").description("결과 코드"),
-                                fieldWithPath("data.insightId").description("생성된 인사이트의 ID"))
+                                fieldWithPath("data.drawerId").description("생성된 서랍의 ID"))
                         .tag("Insight")
                         .build()
         )));
