33ca89dd1a6776b7af6512a0d7cfcdce3854e813
heoboseong7 <boseong844@gmail.com>
2022-09-21 19:43:02 +0900

    [Hotfix] drawer 테이블 PK 설정

--- keewe-domain/src/main/resources/dml/issue-98_dml.sql
+++ keewe-domain/src/main/resources/dml/issue-98_dml.sql
@@ -7,6 +7,7 @@ CREATE TABLE IF NOT EXISTS `drawer`
     created_at      DATETIME(6)     NOT NULL,
     updated_at      DATETIME(6)     NOT NULL,
 
+    PRIMARY KEY (drawer_id),
     FOREIGN KEY (user_id) REFERENCES `user`(user_id),
     CONSTRAINT `drawer_name_constraint` UNIQUE (user_id, name)
 ) ENGINE = InnoDB DEFAULT CHARSET=utf8mb4;
