afb0b19ace2c7bcc20c2883270bf9a0f431f067f
winter-you <winter.you@kakaoenterprise.com>
2022-09-25 22:58:39 +0900

    param 추가

--- keewe-domain/src/main/java/ccc/keewedomain/repository/insight/InsightRepository.java
+++ keewe-domain/src/main/java/ccc/keewedomain/repository/insight/InsightRepository.java
@@ -6,6 +6,7 @@ import ccc.keewedomain.domain.insight.Insight;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import javax.persistence.LockModeType;
 import java.util.Optional;
@@ -14,7 +15,7 @@ public interface InsightRepository extends JpaRepository<Insight, Long> {
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query(value = "select i from Insight i where i.id = :id")
-    Optional<Insight> findByIdWithReadWriteLock(Long id);
+    Optional<Insight> findByIdWithReadWriteLock(@Param("id") Long id);
 
     default Insight findByIdOrElseThrow(Long id) {
         return findByIdWithReadWriteLock(id).orElseThrow(() -> new KeeweException(KeeweRtnConsts.ERR445));
