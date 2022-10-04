411ece07d47a822cfa4191b21538adb762ef17f3
kimyoungi99 <kimyoungi99@naver.com>
2022-10-04 18:25:55 +0900

    chore : rest doc 생성

--- keewe-api/src/main/java/ccc/keeweapi/dto/insight/ReactRequest.java
+++ keewe-api/src/main/java/ccc/keeweapi/dto/insight/ReactRequest.java
@@ -11,4 +11,13 @@ public class ReactRequest {
     private Long insightId;
     private ReactionType reactionType;
     private Long value;
+
+    public static ReactRequest of(Long insightId, ReactionType reactionType, Long value) {
+        ReactRequest request = new ReactRequest();
+        request.insightId = insightId;
+        request.reactionType = reactionType;
+        request.value = value;
+
+        return request;
+    }
 }
