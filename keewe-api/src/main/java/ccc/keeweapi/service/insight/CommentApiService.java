5f8095253a0916078f106094224614f534dea1d5
heoboseong7 <boseong844@gmail.com>
2022-10-22 14:21:40 +0900

    [Refactor] 대표 댓글 답글 제한 KeeweConsts로 이동, [Fix] @Transactional 추가

--- keewe-api/src/main/java/ccc/keeweapi/service/insight/CommentApiService.java
+++ keewe-api/src/main/java/ccc/keeweapi/service/insight/CommentApiService.java
@@ -14,6 +14,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import static ccc.keewecore.consts.KeeweConsts.REPRESENTATIVE_COMMENT_REPLY_LIMIT;
+
 @Service
 @RequiredArgsConstructor
 public class CommentApiService {
@@ -34,8 +36,6 @@ public class CommentApiService {
     // 모든 댓글에 답글이 없는 경우 작성순 댓글 3개
     @Transactional(readOnly = true)
     public RepresentativeCommentResponse getRepresentativeComments(Long insightId) {
-        final Long replyLimit = 2L;
-
         List<Comment> comments = commentDomainService.getRepresentativeCommentsWithWriter(insightId);
         Map<Long, Long> replyNumberPerParentId = commentDomainService.getReplyNumbers(comments);
         Long total = commentDomainService.getCommentNumberByInsightId(insightId);
@@ -45,7 +45,7 @@ public class CommentApiService {
             replyPerParentId = commentDomainService.getRepliesWithWriter(
                             comments.get(0).getId(),
                             Long.MIN_VALUE,
-                            replyLimit
+                            REPRESENTATIVE_COMMENT_REPLY_LIMIT
                     ).stream()
                     .collect(Collectors.groupingBy(reply -> reply.getParent().getId()));
         }
@@ -53,6 +53,7 @@ public class CommentApiService {
         return commentAssembler.toRepresentativeCommentResponse(comments, replyPerParentId, replyNumberPerParentId, total);
     }
 
+    @Transactional(readOnly = true)
     public List<CommentResponse> getCommentsWithFirstReply(Long insightId, CursorPageable<Long> cPage) {
         List<Comment> comments = commentDomainService.getComments(insightId, cPage);
         Map<Long, Comment> firstReplyPerParentId = commentDomainService.getFirstReplies(comments);
@@ -67,6 +68,7 @@ public class CommentApiService {
                 .collect(Collectors.toList());
     }
 
+    @Transactional(readOnly = true)
     public List<ReplyResponse> getReplies(Long parentId, CursorPageable<Long> cPage) {
         return commentDomainService.getReplies(parentId, cPage).stream()
                 .map(commentAssembler::toReplyResponse)
