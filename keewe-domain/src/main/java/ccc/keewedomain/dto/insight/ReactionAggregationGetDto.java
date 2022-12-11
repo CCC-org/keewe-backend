aa8c83fe9ad0611f503a18d987518c271b1ddb1b
Young-Ki Kim <kimyoungi99@naver.com>
2022-12-12 00:01:02 +0900

    [Feat] Reaction Cache 저장 구조 변경 (#163)
    * feat : 리엑션 캐시 인사이트 별 통합
    
    * feat : reaction repo 예외 처리
    
    * fix : switch문 break 추가...
    - TODO : switch문 중복 리펙토링
    
    * test : react 테스트 작성
    
    * refactor : setByType 리펙토링
    
    * test : apply react 메서드 테스트

--- keewe-domain/src/main/java/ccc/keewedomain/dto/insight/ReactionAggregationGetDto.java
+++ keewe-domain/src/main/java/ccc/keewedomain/dto/insight/ReactionAggregationGetDto.java
@@ -1,11 +1,11 @@
 package ccc.keewedomain.dto.insight;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import ccc.keewedomain.cache.domain.insight.CReactionCount;
+import ccc.keewedomain.persistence.domain.insight.enums.ReactionType;
+import lombok.*;
 
 @Getter
+@EqualsAndHashCode
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(staticName = "of")
 public class ReactionAggregationGetDto {
@@ -15,4 +15,33 @@ public class ReactionAggregationGetDto {
     private Long surprise;
     private Long fire;
     private Long eyes;
+
+    public static ReactionAggregationGetDto createByCnt(CReactionCount cnt) {
+        return ReactionAggregationGetDto.of(cnt.getClap(), cnt.getHeart(), cnt.getSad(), cnt.getSurprise(), cnt.getFire(), cnt.getEyes());
+    }
+
+    public Long getByType(ReactionType reactionType) {
+        Long value = -1L;
+        switch (reactionType) {
+            case CLAP:
+                value = clap;
+                break;
+            case HEART:
+                value =  heart;
+                break;
+            case SAD:
+                value = sad;
+                break;
+            case SURPRISE:
+                value = surprise;
+                break;
+            case FIRE:
+                value = fire;
+                break;
+            case EYES:
+                value = eyes;
+                break;
+        }
+        return value;
+    }
 }
