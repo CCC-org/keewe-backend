99a5a7e7dc39ba9dd254f1ff741cc4747d567643
heoboseong7 <boseong844@gmail.com>
2022-12-20 22:21:07 +0900

    [Feat] 타이틀 데이터 DML 추가

--- keewe-domain/src/main/resources/dml/title_dml.sql
+++ keewe-domain/src/main/resources/dml/title_dml.sql
@@ -1,5 +1,88 @@
-REPLACE INTO title (title_id, category, name, introduction, image, created_at, updated_at)
-VALUES (1, 'SIGNUP', '시작이 반', '회원가입 시',
-        'https://keewe-image-bucket-for-local.s3.ap-northeast-2.amazonaws.com/0cd821e4-9428-4cc4-998a-c331df16adfa-4369df33-07e8-4303-bec1-6577a379edd9-%EA%B9%80%EC%98%81%EA%B8%B0+%EB%AF%BC%EC%A6%9D+copy+(1).jpg',
-        NOW(),
-        NOW())
+# 회원가입 카테고리
+INSERT IGNORE INTO title (title_id, category, name, introduction)
+VALUES (1000, 'SIGNUP', '시작이 반', '회원가입 시');
+
+
+# 인사이트 카테고리
+INSERT IGNORE INTO title (title_id, category, name, introduction)
+VALUES (2000, 'INSIGHT', '위대한 첫 도약', '첫 인사이트 업로드');
+
+INSERT IGNORE INTO title (title_id, category, name, introduction)
+VALUES (2001, 'INSIGHT', '초보 기록가', '인사이트 5개');
+
+INSERT IGNORE INTO title (title_id, category, name, introduction)
+VALUES (2002, 'INSIGHT', '중급 기록가', '인사이트 10개');
+
+INSERT IGNORE INTO title (title_id, category, name, introduction)
+VALUES (2003, 'INSIGHT', '고급 기록가', '인사이트 50개');
+
+INSERT IGNORE INTO title (title_id, category, name, introduction)
+VALUES (2004, 'INSIGHT', '인사이트의 신', '인사이트 100개');
+
+INSERT IGNORE INTO title (title_id, category, name, introduction)
+VALUES (2005, 'INSIGHT', '혼자서도 잘 해요', '챌린지가 아닌 인사이트 3개');
+
+
+# 팔로워 카테고리
+INSERT IGNORE INTO title (title_id, category, name, introduction)
+VALUES (3000, 'FOLLOWER', '두근두근 첫만남', '첫 팔로워');
+
+INSERT IGNORE INTO title (title_id, category, name, introduction)
+VALUES (3001, 'FOLLOWER', '자타공인 인기인', '팔로워 10명');
+
+INSERT IGNORE INTO title (title_id, category, name, introduction)
+VALUES (3002, 'FOLLOWER', '피리부는 사나이', '팔로워 100명');
+
+INSERT IGNORE INTO title (title_id, category, name, introduction)
+VALUES (3003, 'FOLLOWER', '정이 많은', '팔로잉 40명');
+
+
+# 반응 카테고리
+INSERT IGNORE INTO title (title_id, category, name, introduction)
+VALUES (4000, 'REACTION', '참 잘했어요', '첫 반응');
+
+INSERT IGNORE INTO title (title_id, category, name, introduction)
+VALUES (4001, 'REACTION', '아낌없이 주는 나무', '게시글 50개에 반응 누를 시');
+
+INSERT IGNORE INTO title (title_id, category, name, introduction)
+VALUES (4002, 'REACTION', '키위새들의 픽', '한 게시물에서 10명에게 반응 얻을 시');
+
+
+# 챌린지 카테고리
+INSERT IGNORE INTO title (title_id, category, name, introduction)
+VALUES (5000, 'CHALLENGE', '챌린지 메이커', '첫 챌린지 생성');
+
+INSERT IGNORE INTO title (title_id, category, name, introduction)
+VALUES (5001, 'CHALLENGE', '실패는 성공의 어머니', '첫 챌린지 실패');
+
+INSERT IGNORE INTO title (title_id, category, name, introduction)
+VALUES (5002, 'CHALLENGE', '첫번째 완주', '첫 챌린지 성공');
+
+INSERT IGNORE INTO title (title_id, category, name, introduction)
+VALUES (5003, 'CHALLENGE', '쉬지않고 도전하는', '두번째 챌린지');
+
+
+# 공유 카테고리
+INSERT IGNORE INTO title (title_id, category, name, introduction)
+VALUES (6000, 'SHARE', '혼자 보기 아까운', '누적 공유 10번');
+
+
+# 북마크 카테고리
+INSERT IGNORE INTO title (title_id, category, name, introduction)
+VALUES (7000, 'BOOKMARK', '인사이트 수집가', '첫 북마크 저장');
+
+INSERT IGNORE INTO title (title_id, category, name, introduction)
+VALUES (7001, 'BOOKMARK', '간직하고 싶은 인사이트', '타인이 내 인사이트를 첫 북마크');
+
+
+# 친구 초대 카테고리
+INSERT IGNORE INTO title (title_id, category, name, introduction)
+VALUES (8000, 'FRIEND_INVITATION', '함께하는 즐거움', '첫 친구 초대');
+
+INSERT IGNORE INTO title (title_id, category, name, introduction)
+VALUES (8001, 'FRIEND_INVITATION', '마당발', '친구 10명 초대');
+
+
+# 키위 타이틀 카테고리
+INSERT IGNORE INTO title (title_id, category, name, introduction)
+VALUES (8000, 'KEEWE_TITLE', 'Shall We Keewe?', '모든 타이틀 달성');
