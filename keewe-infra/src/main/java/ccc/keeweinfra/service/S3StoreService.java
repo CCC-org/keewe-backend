2b97e9c8f7d5c139a9f686dcee1987afdf62498c
kimyoungi99 <kimyoungi99@naver.com>
2023-01-08 16:47:25 +0900

    feat : 이미지 업로드 전 정사각형으로 450 * 450으로 자름

--- keewe-infra/src/main/java/ccc/keeweinfra/service/S3StoreService.java
+++ keewe-infra/src/main/java/ccc/keeweinfra/service/S3StoreService.java
@@ -2,14 +2,23 @@ package ccc.keeweinfra.service;
 
 import ccc.keewecore.consts.KeeweRtnConsts;
 import ccc.keewecore.exception.KeeweException;
+import com.amazonaws.auth.PropertiesCredentials;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
+import org.imgscalr.Scalr;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.UUID;
@@ -29,8 +38,16 @@ public class S3StoreService implements StoreService {
 
         ObjectMetadata objMeta = new ObjectMetadata();
         try {
-            objMeta.setContentLength(multipartFile.getInputStream().available());
-            amazonS3.putObject(bucket, s3FileName, multipartFile.getInputStream(), objMeta);
+            BufferedImage bufferedImage = ImageIO.read(multipartFile.getInputStream());
+            bufferedImage = resizeImage(bufferedImage, 450, 450);
+
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            ImageIO.write(bufferedImage, "jpeg", out);
+            byte[] bytes = out.toByteArray();
+            InputStream imageInputStream = new ByteArrayInputStream(bytes);
+
+            objMeta.setContentLength(bytes.length);
+            amazonS3.putObject(bucket, s3FileName, imageInputStream, objMeta);
         } catch (IOException e) {
             throw new KeeweException(KeeweRtnConsts.ERR600);
         }
@@ -55,5 +72,13 @@ public class S3StoreService implements StoreService {
 
     private String createFileName(String originalFileName) {
         return UUID.randomUUID() + "-" + originalFileName;
+    }
+
+    private BufferedImage resizeImage(BufferedImage originalImage, Integer targetWidth, Integer targetHeight) {
+        Integer imageLength = Math.min(originalImage.getHeight(), originalImage.getWidth());
+        BufferedImage scaledImage =
+                Scalr.crop(originalImage, (originalImage.getWidth() - imageLength) / 2,
+                        (originalImage.getHeight() - imageLength) / 2, imageLength, imageLength);
+        return Scalr.resize(scaledImage, Scalr.Method.AUTOMATIC, Scalr.Mode.FIT_EXACT, targetWidth, targetHeight, Scalr.OP_ANTIALIAS);
     }
 }
