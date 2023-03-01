28a26673bc012c85da2ea70a0ba4c3b1b61228e9
heoboseong7 <boseong844@gmail.com>
2023-03-01 21:59:03 +0900

    [Feat] FK 추가 SQL 작성

---
+++ keewe-domain/src/main/resources/dml/issue-230_dml.sql
@@ -0,0 +1,10 @@
+ALTER TABLE `reaction`
+    ADD CONSTRAINT FOREIGN KEY (insight_id) REFERENCES `insight`(insight_id),
+    ADD CONSTRAINT FOREIGN KEY (reaction_id) REFERENCES `user`(user_id);
+
+ALTER TABLE `reaction_aggregation`
+    ADD CONSTRAINT FOREIGN KEY (insight_id) REFERENCES `insight`(insight_id);
+
+ALTER TABLE `bookmark`
+    ADD CONSTRAINT FOREIGN KEY (user_id) REFERENCES `user`(user_id),
+    ADD CONSTRAINT FOREIGN KEY (insight_id) REFERENCES `insight`(insight_id);
