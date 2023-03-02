6487dce02af096b4cf4190fa5f66b6bac8f4c988
heoboseong7 <boseong844@gmail.com>
2023-03-02 19:42:02 +0900

    [Hotfix] reactor_id 오타 수정

--- keewe-domain/src/main/resources/dml/issue-230_dml.sql
+++ keewe-domain/src/main/resources/dml/issue-230_dml.sql
@@ -1,6 +1,6 @@
 ALTER TABLE `reaction`
     ADD CONSTRAINT FOREIGN KEY (insight_id) REFERENCES `insight`(insight_id),
-    ADD CONSTRAINT FOREIGN KEY (reaction_id) REFERENCES `user`(user_id);
+    ADD CONSTRAINT FOREIGN KEY (reactor_id) REFERENCES `user`(user_id);
 
 ALTER TABLE `reaction_aggregation`
     ADD CONSTRAINT FOREIGN KEY (insight_id) REFERENCES `insight`(insight_id);
