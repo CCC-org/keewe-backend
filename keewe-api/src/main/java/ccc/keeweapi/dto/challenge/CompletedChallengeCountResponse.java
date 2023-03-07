5fba94cdc89aebe0ae3ef3a27c886505ba928e89
Youhoseong <h0song@naver.com>
2023-03-07 20:28:06 +0900

    [Feat] 완료된 챌린지 개수 조회 DTO

---
+++ keewe-api/src/main/java/ccc/keeweapi/dto/challenge/CompletedChallengeCountResponse.java
@@ -0,0 +1,10 @@
+package ccc.keeweapi.dto.challenge;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(staticName = "of")
+public class CompletedChallengeCountResponse {
+    private Long count;
+}
