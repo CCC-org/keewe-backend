e13d1c1fba85190b7ecbd62fbb2851a37ea907e6
heoboseong7 <boseong844@gmail.com>
2023-03-12 18:04:05 +0900

    [Refactor] 챌린지 함께하기(친구) 페이지네이션 문서화용 테스트 변경사항 반영

--- keewe-api/src/test/java/ccc/keeweapi/document/utils/ApiDocumentationTest.java
+++ keewe-api/src/test/java/ccc/keeweapi/document/utils/ApiDocumentationTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
@@ -39,6 +40,7 @@ public class ApiDocumentationTest {
     public void setup(Object controller, RestDocumentationContextProvider provider) {
         mockMvc = MockMvcBuilders.standaloneSetup(controller)
                 .apply(MockMvcRestDocumentation.documentationConfiguration(provider))
+                .setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver())
                 .alwaysDo(print())
                 .alwaysDo(restDocs)
                 .addFilters(new CharacterEncodingFilter("UTF-8", true))
