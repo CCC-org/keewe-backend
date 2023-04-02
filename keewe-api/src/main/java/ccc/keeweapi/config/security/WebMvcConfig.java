d1f281de784d716598a52c97066e86b8788a09df
유호성 <h0song@naver.com>
2023-04-02 21:28:23 +0900

    [NO-ISSUE] USER_ID Logback 로깅 추가 (#274)
    * [NO-ISSUE] USER_ID 키바나 로깅 추가
    
    * [NO-ISSUE] JwtFilter UserId 제거
    
    * [NO-ISSUE] Apply traceId

---
+++ keewe-api/src/main/java/ccc/keeweapi/config/security/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package ccc.keeweapi.config.security;
+
+import ccc.keeweapi.config.interceptor.LoggingInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration(proxyBeanMethods = false)
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+    private final LoggingInterceptor loggingInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor);
+    }
+}
