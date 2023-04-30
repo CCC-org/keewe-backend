4a4377a4b527066f3cf3e04575a4c8a6f4246cbf
Youhoseong <h0song@naver.com>
2023-04-30 19:41:55 +0900

    [Feat] 인사이트 수정 기능 개발

---
+++ keewe-api/src/main/java/ccc/keeweapi/dto/insight/request/InsightUpdateRequest.java
@@ -0,0 +1,19 @@
+package ccc.keeweapi.dto.insight.request;
+
+import ccc.keeweapi.validator.annotations.GraphemeLength;
+import javax.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public class InsightUpdateRequest {
+    private Long insightId;
+
+    @GraphemeLength(min = 1, max = 400)
+    private String contents;
+
+    @Size(min = 1, max = 2000)
+    private String link;
+}
