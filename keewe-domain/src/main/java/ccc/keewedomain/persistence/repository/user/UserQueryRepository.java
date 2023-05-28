5c0536f239dcf8b54ddb38c65e6f48fa0e911a6b
Youhoseong <h0song@naver.com>
2023-05-28 20:33:45 +0900

    탈퇴한 유저 프로필 조회 가능하도록 수정

--- keewe-domain/src/main/java/ccc/keewedomain/persistence/repository/user/UserQueryRepository.java
+++ keewe-domain/src/main/java/ccc/keewedomain/persistence/repository/user/UserQueryRepository.java
@@ -1,23 +1,13 @@
 package ccc.keewedomain.persistence.repository.user;
 
-import ccc.keewedomain.persistence.domain.common.Interest;
-import ccc.keewedomain.persistence.domain.user.Follow;
+import static ccc.keewedomain.persistence.domain.common.QInterest.interest;
+import static ccc.keewedomain.persistence.domain.user.QUser.user;
+
 import ccc.keewedomain.persistence.domain.user.User;
-import ccc.keewedomain.persistence.repository.utils.CursorPageable;
-import com.querydsl.jpa.JPAExpressions;
-import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
-
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
-
-import static ccc.keewedomain.persistence.domain.common.QInterest.interest;
-import static ccc.keewedomain.persistence.domain.title.QTitle.title;
-import static ccc.keewedomain.persistence.domain.user.QFollow.follow;
-import static ccc.keewedomain.persistence.domain.user.QUser.user;
 
 @Repository
 @RequiredArgsConstructor
@@ -31,8 +21,7 @@ public class UserQueryRepository {
                 .from(user)
                 .leftJoin(user.interests, interest)
                 .fetchJoin()
-                .where(user.id.eq(userId)
-                        .and(user.deleted.isFalse()))
+                .where(user.id.eq(userId))
                 .fetchFirst());
     }
 }
