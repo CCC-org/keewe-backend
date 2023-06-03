fce2ab08037d81489432d08d9569bb5e22f02901
Youhoseong <h0song@naver.com>
2023-06-03 15:00:32 +0900

    [Fix] 최초 회원가입 타이틀 획득 버그 수정

--- keewe-api/src/main/java/ccc/keeweapi/controller/api/user/UserSignUpController.java
+++ keewe-api/src/main/java/ccc/keeweapi/controller/api/user/UserSignUpController.java
@@ -6,7 +6,13 @@ import ccc.keewecore.aop.annotations.LocalOnlyApi;
 import ccc.keewedomain.persistence.domain.user.enums.VendorType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -36,6 +42,12 @@ public class UserSignUpController {
     @LocalOnlyApi
     public ApiResponse<?> getToken(@PathVariable Long userId) {
         return ApiResponse.ok(userService.getToken(userId));
+    }
+
+    @GetMapping("/virtual")
+    @LocalOnlyApi
+    public ApiResponse<?> signUpWithVirtualVendor(@RequestParam String code) {
+        return ApiResponse.ok(userService.signupWithOauth(code, VendorType.VIRTUAL));
     }
 
     @PutMapping("/withdraw")
