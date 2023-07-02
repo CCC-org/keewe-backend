c9bdc5a40aa3c7d211dcc6884c85929aaf268731
kimyoungi99 <kimyoungi99@naver.com>
2023-07-02 17:45:28 +0900

    refactor : 함수 모듈화

--- keewe-domain/src/main/java/ccc/keewedomain/common/encryption/AESEncryption.java
+++ keewe-domain/src/main/java/ccc/keewedomain/common/encryption/AESEncryption.java
@@ -1,11 +1,10 @@
 package ccc.keewedomain.common.encryption;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
-import java.security.Key;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import java.util.Base64;
 
 @Component
@@ -20,30 +19,26 @@ public class AESEncryption {
     @Value("${encryption.iv}")
     private String iv;
 
-    public String encrypt(String value) throws Exception {
+    private Cipher initializeCipher(int encryptMode) throws Exception {
         Cipher c = Cipher.getInstance(ALGORITHM);
         SecretKeySpec keySpec = new SecretKeySpec(encryptionKey.getBytes(), "AES");
         IvParameterSpec ivParamSpec = new IvParameterSpec(iv.getBytes());
-        c.init(Cipher.ENCRYPT_MODE, keySpec, ivParamSpec);
+        c.init(encryptMode, keySpec, ivParamSpec);
+        return c;
+    }
+
+    public String encrypt(String value) throws Exception {
+        Cipher c = initializeCipher(Cipher.ENCRYPT_MODE);
         byte[] encValue = c.doFinal(value.getBytes(UNICODE_FORMAT));
         String encryptedValue = Base64.getEncoder().encodeToString(encValue);
-        System.out.println(encryptedValue);
         return encryptedValue;
     }
 
     public String decrypt(String value) throws Exception {
-        Cipher c = Cipher.getInstance(ALGORITHM);
-        SecretKeySpec keySpec = new SecretKeySpec(encryptionKey.getBytes(), "AES");
-        IvParameterSpec ivParamSpec = new IvParameterSpec(iv.getBytes());
-        c.init(Cipher.DECRYPT_MODE, keySpec, ivParamSpec);
+        Cipher c = initializeCipher(Cipher.DECRYPT_MODE);
         byte[] decodedValue = Base64.getDecoder().decode(value);
         byte[] decValue = c.doFinal(decodedValue);
         String decryptedValue = new String(decValue);
         return decryptedValue;
-    }
-
-    private Key generateKey() throws Exception {
-        Key key = new SecretKeySpec(encryptionKey.getBytes(UNICODE_FORMAT), ALGORITHM);
-        return key;
     }
 }
