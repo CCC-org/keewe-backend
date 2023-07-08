c4f4734a42f115f77125a6124889f04696e7649b
Youhoseong <h0song@naver.com>
2023-07-08 17:54:28 +0900

    [Feat] `keewe-event` 모듈 신규 생성

---
+++ keewe-event/src/main/java/ccc/keeweevent/KeeweEventApplication.java
@@ -0,0 +1,13 @@
+package ccc.keeweevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeeweEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeeweEventApplication.class, args);
+	}
+
+}
---
+++ keewe-event/src/test/java/ccc/keeweevent/KeeweEventApplicationTests.java
@@ -0,0 +1,13 @@
+package ccc.keeweevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeeweEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
