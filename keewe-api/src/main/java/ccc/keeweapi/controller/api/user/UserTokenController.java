498589f539db44a75d2cd292f76140d0fa470130
heoboseong7 <boseong844@gmail.com>
2023-07-29 14:31:55 +0900

    [Fix] 토큰 등록 컨트롤러 @RequestBody 추가

--- keewe-api/src/main/java/ccc/keeweapi/controller/api/user/UserTokenController.java
+++ keewe-api/src/main/java/ccc/keeweapi/controller/api/user/UserTokenController.java
@@ -5,6 +5,7 @@ import ccc.keeweapi.dto.user.RegisterPushTokenRequest;
 import ccc.keeweapi.service.user.command.UserCommandApiService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +16,7 @@ public class UserTokenController {
     private final UserCommandApiService userCommandApiService;
 
     @PutMapping("/push")
-    public ApiResponse<Void> registerPushToken(RegisterPushTokenRequest request) {
+    public ApiResponse<Void> registerPushToken(@RequestBody RegisterPushTokenRequest request) {
         userCommandApiService.registerPushToken(request.getPushToken());
         return ApiResponse.ok();
     }
