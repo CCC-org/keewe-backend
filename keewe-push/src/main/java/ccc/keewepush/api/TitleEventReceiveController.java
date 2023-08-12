b132ccf04889fa64a4a2df21f5511a97cb40bf6b
Youhoseong <h0song@naver.com>
2023-08-12 15:34:28 +0900

    [Fix] 스웨거 미노출 버그 수정

--- keewe-push/src/main/java/ccc/keewepush/api/TitleEventReceiveController.java
+++ keewe-push/src/main/java/ccc/keewepush/api/TitleEventReceiveController.java
@@ -5,7 +5,6 @@ import ccc.keewecore.dto.TitleEvent;
 import ccc.keewepush.service.TitleEventService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
@@ -16,10 +15,7 @@ import reactor.core.publisher.Flux;
 public class TitleEventReceiveController {
     private final TitleEventService titleEventService;
 
-    //FIXME userId 시큐리티
-    @GetMapping("/{userId}")
     public Flux<TitleEvent> createEventConnection(@PathVariable Long userId) {
         return titleEventService.createEventAsFlux(userId);
     }
-
 }
