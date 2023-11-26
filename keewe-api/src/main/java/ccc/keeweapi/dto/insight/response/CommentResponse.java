ce78d955f57985144e6433b8fd2f8dbd28109857
Youhoseong <h0song@naver.com>
2023-11-26 16:29:20 +0900

    [Chore] 주석

--- keewe-api/src/main/java/ccc/keeweapi/dto/insight/response/CommentResponse.java
+++ keewe-api/src/main/java/ccc/keeweapi/dto/insight/response/CommentResponse.java
@@ -8,6 +8,7 @@ public interface CommentResponse extends BlockFilteringResponse {
     Long getId();
     String getContent();
     String getCreatedAt();
+    // note. 첫 댓글 1개 응답 (최신 버전 미사용, Native 하위버전 호환을 위해 No Remove)
     List<ReplyResponse> getReplies();
     Long getTotalReply();
 }
